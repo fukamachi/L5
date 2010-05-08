30edfe88a8550f213d29f721f4a05c2e281d1555
fukamachi <e.arrows@gmail.com>
2010-05-08 10:47:53 +0900

    created a project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject scarecrow "1.0.0-SNAPSHOT"
+  :author "深町英太郎(E. Fukamachi)"
+  :description "Yet Another Presentation with Clojure"
+  :url "http://github.com/fukamachi/scarecrow"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]]
+  :repositries [["clojars" "http://clojars.org/repo"]])
---
+++ test/scarecrow/core_test.clj
@@ -0,0 +1,6 @@
+(ns scarecrow.core-test
+  (:use [scarecrow.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))

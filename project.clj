928acade7b49e1b2b61cc805511ef018688ae74b
fukamachi <e.arrows@gmail.com>
2010-05-14 18:16:27 +0900

    added swank-clojure

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :run-aliases {:presen [scarecrow.core]}
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
+  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
+                     [lein-clojars "0.5.0"]
                      [lein-run "1.0.0-SNAPSHOT"]]
   :repositries [["clojars" "http://clojars.org/repo"]])

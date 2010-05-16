694f7c05842b88a2605dc3d884c99d850bc53f7d
fukamachi <e.arrows@gmail.com>
2010-05-16 21:35:42 +0900

    added jline to project.clj for using lein repl with Clojure 1.2

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :run-aliases {:presen [scarecrow.core]}
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
+  :dev-dependencies [[jline/jline "0.9.94"]
+                     [swank-clojure "1.2.0-SNAPSHOT"]
                      [lein-clojars "0.5.0"]
                      [lein-run "1.0.0-SNAPSHOT"]]
   :repositries [["clojars" "http://clojars.org/repo"]])

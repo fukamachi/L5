f76f93b0e913d2a8b1c598e4988b36fcd39e1bbd
fukamachi <e.arrows@gmail.com>
2010-08-11 13:36:46 +0900

    changed to create a repl server

--- src/L5/core.clj
+++ src/L5/core.clj
@@ -1,8 +1,11 @@
 (ns L5.core
   (:use [L5.context :only [make-context start]]
-        [L5.layout])
+        L5.layout
+        clojure.contrib.server-socket)
   (:require [L5.slide :as s])
   (:import [java.awt Font]))
+
+(def *server-socket* (create-repl-server 12345 25))
 
 (load-file "init.clj")
 

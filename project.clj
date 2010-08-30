2fdcb253ededade8892bc7cd37a01c3d40694fc2
fukamachi <e.arrows@gmail.com>
2010-08-30 16:04:59 +0900

    moved lein-run to :dev-dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :url "http://github.com/fukamachi/L5"
   :run-aliases {:presen [L5.core], :export [L5.export]}
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [lein-run "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[jline/jline "0.9.94"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]
+                     [jline/jline "0.9.94"]
                      [swank-clojure "1.2.0-SNAPSHOT"]
                      [lein-clojars "0.5.0"]]
   :repositries [["clojars" "http://clojars.org/repo"]])

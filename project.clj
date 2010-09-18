f04a6a3bb45ff897fda03750a1910b1dab6e8cf3
fukamachi <e.arrows@gmail.com>
2010-09-18 11:18:16 +0900

    changed to download iText from it's maven repos

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :url "http://github.com/fukamachi/L5"
   :run-aliases {:presen [L5.core], :export [L5.export]}
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [com.itextpdf/itextpdf "5.0.4"]]
   :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]
                      [jline/jline "0.9.94"]
                      [swank-clojure "1.2.0-SNAPSHOT"]
                      [lein-clojars "0.5.0"]]
-  :repositries [["clojars" "http://clojars.org/repo"]])
+  :repositories {"clojars" "http://clojars.org/repo"
+                 "itextpdf" "http://maven.itextpdf.com/"})

221473dac1cba699f7498c0c51e6fd104b4d8934
fukamachi <e.arrows@gmail.com>
2010-09-21 16:52:22 +0900

    added License info to project.clj and added exclusions for standalone jar

--- project.clj
+++ project.clj
@@ -2,12 +2,19 @@
   :author "深町英太郎(E. Fukamachi)"
   :description "Yet Another Presentation with Clojure"
   :url "http://github.com/fukamachi/L5"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"
+            :distribution :repo}
   :main L5.core
   :run-aliases {:presen [L5.core -main], :export [L5.core -main "export"]}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.itextpdf/itextpdf "5.0.4"]]
+                 [com.itextpdf/itextpdf "5.0.4"
+                  :exclusions [org.bouncycastle/bcmail-jdk14
+                               org.bouncycastle/bcprov-jdk14
+                               org.bouncycastle/bctsp-jdk14]]]
   :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]
                      [lein-clojars "0.5.0"]]
   :repositories {"clojars" "http://clojars.org/repo"
-                 "itextpdf" "http://maven.itextpdf.com/"})
+                 "itextpdf" "http://maven.itextpdf.com/"}
+  :uberjar-name "L5.jar")
